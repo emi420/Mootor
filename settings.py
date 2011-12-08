293eea1dfdae797efb54b2e353cf908b1616be77
nn <emi420@li392-212.members.linode.com>
2011-12-07 19:45:41 -0500

    todo configurado, funcionando en produccion

--- settings.py
+++ settings.py
@@ -50,7 +50,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8000/ucore/uploads/'
+MEDIA_URL = 'http://50.116.1.212:8000/uploads/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/xampp/htdocs/ufront/static',
+    '/var/www/ufront/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -108,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/xampp/htdocs/ufront'
+    '/var/www/ufront'
 )
 
 INSTALLED_APPS = (
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'site',
+    'ufront.site',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
  
