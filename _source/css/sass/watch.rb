50bd6be1ee1f61267aa154dd4e8521b0fc20bef8
Natalia Petit <natalia.petit@voolks.com>
2012-07-18 19:25:18 -0300

    changed css3 animation in moo-ui-loading, added basic styling

---
+++ _source/css/sass/watch.rb
@@ -0,0 +1,17 @@
+# Script to watch a directory for any changes to a haml file
+# and compile it.
+#
+# USAGE: ruby haml_watch.rb <directory_to_watch>
+#  
+require 'rubygems'
+require 'fssm'
+
+directory = ARGV.first
+FSSM.monitor(directory, '*.scss') do
+  update do |base, relative|
+    command = "cd ../../../css/src/; compass compile -f; cd ../../_source/css/sass"
+    %x{#{command}}
+    puts "Updated"
+  end
+end
+
