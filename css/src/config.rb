9b5ef17f479a46d0629b654b5d7dfc39e1e832f3
Natalia Petit <natalia.petit@voolks.com>
2012-07-30 16:43:13 -0300

    fixed minor bugs, edited comments, added spriting test

--- css/src/config.rb
+++ css/src/config.rb
@@ -1,9 +1,9 @@
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "."
 css_dir = "../"
-sass_dir = "."
-images_dir = "../../img"
+sass_dir = ""
+images_dir = "../img"
 javascripts_dir = "../../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
