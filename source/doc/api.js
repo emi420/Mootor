84ef02eda5c4ab126e9913690ec1442e1ce80d2e
@martinszy <martinsz@gmail.com>
2014-01-10 17:50:06 -0300

    Adding doc folder with automatically generated documentation for the JS source. Generate using

---
+++ source/doc/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "$.app",
+        "app.View",
+        "panel"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
