54c113f84c180d663a0ae7e8cfb7955df888cc78
Emilio Mariscal <emi420@gmail.com>
2014-02-14 12:13:27 -0300

    Documentation update 2/2

---
+++ api.js
@@ -0,0 +1,55 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "App",
+        "Context",
+        "Event",
+        "Route",
+        "Router",
+        "UI",
+        "UIApp",
+        "UIHeader",
+        "UILoading",
+        "UINavBar",
+        "UINavItem",
+        "UIPanel",
+        "UIView",
+        "View",
+        "window.m"
+    ],
+    "modules": [
+        "App",
+        "Mootor",
+        "Router",
+        "UI",
+        "View"
+    ],
+    "allModules": [
+        {
+            "displayName": "App",
+            "name": "App",
+            "description": "The App class defines the main object of the applications"
+        },
+        {
+            "displayName": "Mootor",
+            "name": "Mootor",
+            "description": "The Mootor module handles the creation of App and Namespaces"
+        },
+        {
+            "displayName": "Router",
+            "name": "Router",
+            "description": "The Route class is for defining a route"
+        },
+        {
+            "displayName": "UI",
+            "name": "UI",
+            "description": "The UI class is the class for all user interface elements.\nIt is not directly used, but extended by many other classes."
+        },
+        {
+            "displayName": "View",
+            "name": "View",
+            "description": "The View class handles each screen of the application. \nA list of views is specified in the applications options\nand the files are loaded from the views/ folder.\nEach view has a viewName.js and a viewName.html file.\nThe viewName.js file defines options for the view."
+        }
+    ]
+} };
+});
