c3af08462802208c411a142cc46ce2ee75a90cc1
Martín Szyszlican <martinsz@gmail.com>
2014-02-18 14:40:37 -0300

    Jasmine: All tests are spec'd out.

--- source/spec/AppSpec.js
+++ source/spec/AppSpec.js
@@ -4,6 +4,8 @@ var app,
 
 window._appIsInitTest = false;
 
+window.location.hash = "";
+
 var createApp = function(done) {
 	if (typeof m.app === "function") {
 		app = m.app({
@@ -135,59 +137,7 @@ describe("App", function() {
 
 
 		});
-        
-		it("Should be able to detect if it is running in a browser", function(done) {
-            // Except context().browser to be true
-            expect(typeof m.context.browser).toBe("string");
 
-            done();
-
-		});
-
-	});
-
-
-	describe("I want to run my app a as native app", function() {
-		beforeEach(createApp);
-		xit("Should be able to detect it is running in PhoneGap / Cordova", function(done) {
-            // Except context().cordova or context().phonegap to be true
-            expect(m.context.cordova || m.context.phonegap).toBe(true);
-
-            done();
-
-		});
-		it("Should be able to detect device vendor", function(done) {
-            // Except context().device.vendor
-            expect(m.context.device.vendor).toBeDefined();
-
-            done();
-
-		});
-		xit("Should be able to detect hardware buttons", function(done) {
-            // Except context().device.backButton
-            expect(m.context.device.backButton).toBeDefined();
-
-            done();
-
-		});
-		xit("Should be able to define a method callback run on back button event", function(done) {
-            // TODO
-
-            done();
-
-		});
-		xit("Should be able to define a method callback run on home button event", function(done) {
-            // TODO
-
-            done();
-
-		});
-		xit("Should be able to define a method callback run on menu button event", function(done) {
-            // TODO
-
-            done();
-
-		});
 	});
 
 
@@ -283,7 +233,7 @@ describe("App", function() {
 });
 
 
-describe("App Async 2", function() {
+describe("App Links", function() {
 
 	describe("I want to add a link to another view with parameters", function() {
 
@@ -297,7 +247,7 @@ describe("App Async 2", function() {
 
             
 			expect(
-                view.params[0]
+                view.params()[0]
             ).toBe("10");
             
             done();
--- source/spec/MootorSpec.js
+++ source/spec/MootorSpec.js
@@ -1,30 +1,62 @@
 describe("Mootor", function() {
 
-	describe("I want to know viewport properties", function() {
+	describe("I want to run my app a as web app", function() {
+		beforeEach(createApp);
 
-		xit("Should be able to get viewport's height", function() {
+        
+		it("Should be able to detect if it is running in a browser", function(done) {
+            // Except context().browser to be true
+            expect(typeof m.context.browser).toBe("string");
+
+            done();
 
 		});
-		xit("Should be able to get viewport's width", function() {
 
-		});
 	});
 
 
-	describe("I want to take action when the viewport is resized - (when this case occurs on mobile?)", function() {
+	describe("I want to run my app a as native app", function() {
+		beforeEach(createApp);
+		it("Should be able to detect it is running in PhoneGap / Cordova", function(done) {
+            // Except context().cordova or context().phonegap to be true
+            expect(m.context.cordova || m.context.phonegap).toBe(true);
 
-		xit("Should be able to set an event callback when the viewport is resized", function() {
-
-		});
-	});
-
-
-	describe("I want to take action when the device is rotated", function() {
-
-		xit("Should be able to set an event callback when the device is rotated", function() {
+            done();
 
 		});
+		it("Should be able to detect device vendor", function(done) {
+            // Except context().device.vendor
+            expect(m.context.device.vendor).toBeDefined();
 
+            done();
+
+		});
+		it("Should be able to detect hardware buttons", function(done) {
+            // Except context().device.backButton
+            expect(m.context.device.backButton).toBeDefined();
+
+            done();
+
+		});
+
+		it("Should be able to define a method callback run on back button event", function(done) {
+            // TODO
+
+            done();
+
+		});
+		it("Should be able to define a method callback run on home button event", function(done) {
+            // TODO
+
+            done();
+
+		});
+		it("Should be able to define a method callback run on menu button event", function(done) {
+            // TODO
+
+            done();
+
+		});
 	});
 });
 
