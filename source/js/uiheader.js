aad72cc7403c3859cf131fff01a598ad2ea94f98
Emilio Mariscal <emi420@gmail.com>
2014-03-09 18:04:36 -0300

    + UIHeader working OK

--- source/js/uiheader.js
+++ source/js/uiheader.js
@@ -15,16 +15,70 @@
 
     var UIHeader,
     
-        UINavBar;
+        UINavBar,
+        UIView,
+        View,
+        UIApp;
 
     // Dependences
 
+    View = Mootor.View;
     UINavBar = Mootor.UINavBar;
+    UIView = Mootor.UIView;
+    UIApp = Mootor.UIApp;
+    
+    // Event handlers
+
+    UIApp.on("init", function(self) {
+
+        // FIXME CHECK (parentElement?)
+        var headerEl = self.el.parentElement.getElementsByTagName("header")[0];
+
+        if (headerEl) {
+            self.header = new UIHeader({
+                el: headerEl
+            });
+        }
+    });
+
+    UIView.on("init", function(self) {
+        var headerEl = self.panel.el.getElementsByTagName("header")[0];
+
+        if (headerEl) {
+            self.panel.el.removeChild(headerEl);
+
+            self.header = new UIHeader({
+                el: headerEl
+            });
+
+            self.view.on("load", function(self) {
+                // FIXME CHECK (parentElement?)
+                var headerEl = m.app.ui.el.parentElement.getElementsByTagName("header")[0];
+                m.app.ui.el.parentElement.replaceChild(
+                    self.ui.header.el,
+                    headerEl
+                )
+            });
+        } else {
+
+            self.view.on("load", function(self) {
+                // FIXME CHECK (parentElement?)
+                var headerEl = m.app.ui.el.parentElement.getElementsByTagName("header")[0];
+                m.app.ui.el.parentElement.replaceChild(
+                    m.app.ui.header.el,
+                    headerEl
+                )
+            });
+
+
+        }
+    });
 
     // Private constructors
 
-    UIHeader = Mootor.UIHeader = function() {
-        // code here
+    UIHeader = Mootor.UIHeader = function(options) {
+        this.el = options.el;
+        this.$el = $(this.el);
     };
 
     // Prototypal inheritance
@@ -53,7 +107,12 @@
         * @chainable
         */
         title: function(title) {
-            
+            var titleEl = this.el.getElementsByTagName("h1")[0];
+            if (title !== undefined) {
+                titleEl.innerHTML = title;
+            } else {
+                return titleEl.innerHTML;
+            }
         }
     });  
           
