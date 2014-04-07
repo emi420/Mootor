1740bef7ce9aab8810e53455c952c00dc7ce1d37
@martinszy <martinsz@gmail.com>
2014-04-07 19:11:16 -0300

    Adding UIButton class (handles adding aria-role to buttons) Adding the first version of the auto-generated sprite

--- source/css/config.rb
+++ source/css/config.rb
@@ -1,13 +1,13 @@
 # Set this to the root of your project when deployed:
 css_dir = "../../dist/css"
 sass_dir = "."
-#images_dir = "../../img"
+images_dir = "../img/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded # :compressed # :expanded or :nested or :compact or 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 #line_comments = false
