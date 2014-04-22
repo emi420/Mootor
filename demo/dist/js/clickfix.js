5ecd9c10463292ae6fcb5771145d143e62db1415
Emilio Mariscal <emi420@gmail.com>
2014-04-22 16:42:48 -0300

    Updated demo

---
+++ demo/dist/js/clickfix.js
@@ -0,0 +1,16 @@
+(function($) {
+
+    // Allow to use "click" or tap and 
+    // not worry about device capabilities
+    
+    if ( !!('ontouchstart' in window) ) {
+        $.fn._on = $.fn.on;
+        $.fn.on = function(event, selector, data, callback, one) {
+            if (event.indexOf("click") > -1) {
+                event = event.replace("click","");
+            }
+            return $.fn._on.call(this, event, selector, data, callback, one);
+        };
+    }
+
+}(window.$))
