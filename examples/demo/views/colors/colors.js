bef6515197b151bd0ea417bbc7a0dd75b3b28ab8
@martinszy <martinsz@gmail.com>
2014-05-05 22:13:10 +0000

    Fixed text color in colors demo and added color names

--- examples/demo/views/colors/colors.js
+++ examples/demo/views/colors/colors.js
@@ -5,16 +5,26 @@ for (c in colors_vary) {
 	$line = $("<div></div>").addClass("color-line");
 	$("#colors_container").append($line);
 
-	$div = $("<div></div>").addClass("color m-bg-color-"+color+"-darker").html(color);
-	$line.append($div);
-	$div = $("<div></div>").addClass("color m-bg-color-"+color+"-dark").html(color);
-	$line.append($div);
-	$div = $("<div></div>").addClass("color m-bg-color-"+color).html(color);
-	$line.append($div);
-	$div = $("<div></div>").addClass("color m-bg-color-"+color+"-light").html(color);
+	classes = "m-bg-color-"+color+"-darker  m-color-white";
+	$div = $("<div></div>").addClass("color "+classes).html(classes);
 	$line.append($div);
 
-	$div = $("<div></div>").addClass("color m-bg-color-"+color+"-lighter").html(color);
+	classes = "m-bg-color-"+color+"-dark  m-color-white";
+	$div = $("<div></div>").addClass("color "+classes).html(classes);
+	$line.append($div);
+
+	classes = "m-bg-color-"+color+"-darker ";
+	classes += (color == "warning" ? "m-color-black" : "m-color-white")
+	$div = $("<div></div>").addClass("color "+classes).html(classes);
+	$line.append($div);
+
+	classes = "m-bg-color-"+color+"-light ";
+	classes += (color == "danger" ? "m-color-white" : "m-color-black")
+	$div = $("<div></div>").addClass("color "+classes).html(classes);
+	$line.append($div);
+
+	classes = "m-bg-color-"+color+"-lighter m-color-black";
+	$div = $("<div></div>").addClass("color "+classes).html(classes);
 	$line.append($div);
 
 }
@@ -24,7 +34,7 @@ $("#colors_container").append($line);
 
 colors_static = ["white","cream","black","transparent"];
 for (c in colors_static) {
-	color = colors_static[c];
-	$div = $("<div></div>").addClass("color m-bg-color-"+color).html(color);
+	classes = "m-bg-color-"+colors_static[c] + " " + (colors_static[c] != "black" ? "m-color-black" : "m-color-white")
+	$div = $("<div></div>").addClass("color  " + classes).html(classes);
 	$line.append($div);
 }
