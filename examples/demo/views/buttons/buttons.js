d24542414f95ffd18c47151c289a35c3db70d73a
@martinszy <martinsz@gmail.com>
2014-05-08 00:09:16 +0000

    Uncommited work on buttons after the GIT CRASH.

--- examples/demo/views/buttons/buttons.js
+++ examples/demo/views/buttons/buttons.js
@@ -4,21 +4,22 @@ button_width = ["m-button","m-button-block","m-button-fullwidth"]
 button_style = ["m-button","m-button-outline","m-button-clear"]
 
 
-for (c in button_colors) {
-	color = button_colors[c];
 	
-	for (s in button_sizes) {
-		size = button_sizes[s];
+for (s in button_sizes) {
+	size = button_sizes[s];
 
-		for (w in button_width) {
-			width = button_width[w];
+	for (w in button_width) {
+		width = button_width[w];
+
+		for (t in button_style) {
+			style = button_style[t];
 
 			$line = $("<div></div>").addClass("button-line");
-			$("#buttons_container_"+width).append($line);
+			$("#buttons_container").append($line);
 
 
-			for (t in button_style) {
-				style = button_style[t];
+			for (c in button_colors) {
+				color = button_colors[c];
 
 				$div = $("<div></div>")
 					.addClass(color)
