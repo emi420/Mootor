590353f08b33f1777fe44ed5bc4e166aeaf2b17d
Emilio Mariscal <emi420@gmail.com>
2014-07-23 19:23:32 -0300

    Demo update

--- demo/views/form/form.js
+++ demo/views/form/form.js
@@ -3,4 +3,18 @@ m.app.view("form").on("ready", function() {
       alert("Not implemented on this demo.")
       return false; 
    });
+   
+   // FIXME CHECK
+   // Firefox OS fix
+   $("input.m-text").on("blur", function() {
+       window.setTimeout(function() {
+           window.document.body.scrollTop = 0;
+       }, 200);
+   });   
+   $("textarea").on("blur", function() {
+       window.setTimeout(function() {
+           window.document.body.scrollTop = 0;
+       }, 200);
+   });
+   
 });
