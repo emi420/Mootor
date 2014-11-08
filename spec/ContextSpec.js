52b3ac82a0176bd2ddf5353a78e8b5ae6ca47f18
Emilio Mariscal <emi420@gmail.com>
2014-11-08 13:26:17 -0300

    Updated specs

--- spec/ContextSpec.js
+++ spec/ContextSpec.js
@@ -14,16 +14,6 @@ describe("Context", function() {
 		});
 	});
 
-
-	describe("I want to take action when the device is rotated", function() {
-
-		xit("Should be able to set an event callback when the device is rotated", function() {
-
-		});
-
-	});
-
-
 	describe("I want to run my app a as native app", function() {
 		beforeEach(createApp);
 		it("Should be able to detect it is running in PhoneGap / Cordova", function(done) {
@@ -33,32 +23,7 @@ describe("Context", function() {
             done();
 
 		});
-		xit("Should be able to detect hardware buttons", function(done) {
-            // Except context().device.backButton
-            expect(m.context.device.backButton).toBeDefined();
 
-            done();
-
-		});
-
-		xit("Should be able to define a method callback run on back button event", function(done) {
-            // TODO
-
-            done();
-
-		});
-		xit("Should be able to define a method callback run on home button event", function(done) {
-            // TODO
-
-            done();
-
-		});
-		xit("Should be able to define a method callback run on menu button event", function(done) {
-            // TODO
-
-            done();
-
-		});
 	});
 
 });
